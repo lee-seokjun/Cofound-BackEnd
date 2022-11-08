4f600b706bac6377e904e4529528812674d1ae7f
seoklee <iope100@naver.com>
2022-11-08 19:00:11 +0900

    Initial commit

---
+++ src/test/java/com/cofound/gateway/CofoundGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cofound.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofoundGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
