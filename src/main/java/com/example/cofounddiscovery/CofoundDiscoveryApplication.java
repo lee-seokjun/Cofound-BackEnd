a4302295444b255b07dbc5bd0b181167f8f65597
seoklee <iope100@naver.com>
2022-11-08 17:22:30 +0900

    add eureka server

--- src/main/java/com/example/cofounddiscovery/CofoundDiscoveryApplication.java
+++ src/main/java/com/example/cofounddiscovery/CofoundDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.example.cofounddiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CofoundDiscoveryApplication {
 
 	public static void main(String[] args) {
