9d985cac1ee13ff3cca7f101e99bdeadab701e63
seoklee <iope100@naver.com>
2022-11-08 18:08:39 +0900

    Initial commit

---
+++ src/main/java/com/cofound/config/CofoundConfigApplication.java
@@ -0,0 +1,15 @@
+package com.cofound.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CofoundConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofoundConfigApplication.class, args);
+	}
+
+}
