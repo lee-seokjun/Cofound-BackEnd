043c65112d29b6d29aab08b8b8b2fdc5a72a980c
seoklee <iope100@naver.com>
2022-11-08 17:15:08 +0900

    init

---
+++ src/main/java/com/example/cofounddiscovery/CofoundDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.cofounddiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofoundDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofoundDiscoveryApplication.class, args);
+	}
+
+}
